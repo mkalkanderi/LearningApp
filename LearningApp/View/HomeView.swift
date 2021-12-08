ff7271e46d6f1a6f8598a4c75d8dc1f72cb3d97d
Mohammad Alkandari <mohammedalkandari8@gmail.com>
2021-12-08 15:25:03 +0300

    Updated to lesson 7

--- LearningApp/View/HomeView.swift
+++ LearningApp/View/HomeView.swift
@@ -5,17 +5,64 @@
 //  Created by Mohd on 28/11/2021.
 //
 
+
 import SwiftUI
 
 struct HomeView: View {
+    
+    @EnvironmentObject var model: ContentModel
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        NavigationView {
+            VStack (alignment: .leading) {
+                Text("What do you want to do today?")
+                    .padding(.leading, 20)
+                
+                ScrollView {
+                    
+                    LazyVStack {
+                        
+                        ForEach(model.modules) { module in
+                            
+                            VStack (spacing: 20) {
+                                
+                                NavigationLink(
+                                    destination:
+                                        ContentView()
+                                        .onAppear(perform: {
+                                            model.beginModule(module.id)
+                                        }),
+                                    label: {
+                                        
+                                        // Learning Card
+                                        HomeViewRow(image: module.content.image, title: "Learn \(module.category)", description: module.content.description, count: "\(module.content.lessons.count) Lessons", time: module.content.time)
+                                        
+                                    })
+                                
+                                
+                                
+                                
+                                // Test Card
+                                HomeViewRow(image: module.test.image, title: "\(module.category) Test", description: module.test.description, count: "\(module.test.questions.count) Lessons", time: module.test.time)
+                            }
+                        }
+                        
+                    }
+                    .accentColor(.black)
+                    .padding()
+                    
+                }
+            }
+            .navigationTitle("Get Started")
+        }
+        .navigationViewStyle(.stack)
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView()
+            .environmentObject(ContentModel())
     }
 }
--- LearningApp/ViewModel/ContentModel.swift
+++ LearningApp/ViewModel/ContentModel.swift
@@ -9,34 +9,119 @@ import Foundation
 
 class ContentModel: ObservableObject {
     
+    // List of modules
     @Published var modules = [Module]()
+    
+    // Current module
+    @Published var currentModule: Module?
+    var currentModuleIndex = 0
+    
+    // Current lesson
+    @Published var currentLesson: Lesson?
+    var currentLessonIndex = 0
+    
     var styleData: Data?
     
-    init(){
+    init() {
+        
         getLocalData()
+        
     }
     
-    func getLocalData(){
+    // MARK: - Data methods
+    
+    func getLocalData() {
+        
+        // Get a url to the json file
         let jsonUrl = Bundle.main.url(forResource: "data", withExtension: "json")
         
         do {
+            // Read the file into a data object
             let jsonData = try Data(contentsOf: jsonUrl!)
+            
+            // Try to decode the json into an array of modules
             let jsonDecoder = JSONDecoder()
             let modules = try jsonDecoder.decode([Module].self, from: jsonData)
+            
+            // Assign parsed modules to modules property
             self.modules = modules
-        } catch {
-            print("error parsing local data")
-            print(error)
+        }
+        catch {
+            // TODO log error
+            print("Couldn't parse local data")
         }
         
+        // Parse the style data
         let styleUrl = Bundle.main.url(forResource: "style", withExtension: "html")
         
         do {
+            
+            // Read the file into a data object
             let styleData = try Data(contentsOf: styleUrl!)
+            
             self.styleData = styleData
-        } catch {
-            print("error parsing style data")
+        }
+        catch {
+            // Log error
+            print("Couldn't parse style data")
         }
         
+    }
+    
+    // MARK: - Module navigation methods
+    
+    func beginModule(_ moduleid:Int) {
+        
+        // Find the index for this module id
+        for index in 0..<modules.count {
+            
+            if modules[index].id == moduleid {
+                
+                // Found the matching module
+                currentModuleIndex = index
+                break
+            }
+        }
+        
+        // Set the current module
+        currentModule = modules[currentModuleIndex]
+    }
+    
+    func beginLesson(_ lessonIndex:Int) {
+        
+        // Check that the lesson index is within range of module lessons
+        if lessonIndex < currentModule!.content.lessons.count {
+            currentLessonIndex = lessonIndex
+        }
+        else {
+            currentLessonIndex = 0
+        }
+        
+        // Set the current lesson
+        currentLesson = currentModule!.content.lessons[currentLessonIndex]
+        
+    }
+    
+    func nextLesson() {
+        
+        // Advance the lesson index
+        currentLessonIndex += 1
+        
+        // Check that it is within range
+        if currentLessonIndex < currentModule!.content.lessons.count {
+            
+            // Set the current lesson property
+            currentLesson = currentModule!.content.lessons[currentLessonIndex]
+        }
+        else {
+            // Reset the lesson state
+            currentLessonIndex = 0
+            currentLesson = nil
+        }
+    }
+    
+    func hasNextLesson() -> Bool {
+        
+        return (currentLessonIndex + 1 < currentModule!.content.lessons.count)
     }
 }
